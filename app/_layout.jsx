2707f6c57ee7ac11d5b90a092366f3e380c34796
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-05-31 15:52:26 +0700

    feat: auth implementation

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts, Raleway_700Bold } from "@expo-google-fonts/raleway";
+import { UrlProvider } from "../context/UrlProvider";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -24,33 +25,35 @@ const RootLayout = () => {
   if (!fontsLoaded && !error) return null;
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="(auth)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="(profile)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="(focus)"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <UrlProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(auth)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="(profile)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="(focus)"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </UrlProvider>
   );
 };
 
