8e7cd08110f9d98e74ab627331835c9431d62c33
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-06-11 10:57:30 +0700

    unregister leaderboard from root layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -53,12 +53,6 @@ const InitialLayout = () => {
         }}
       />
       <Stack.Screen
-        name="(leaderboard)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
         name="+not-found"
         options={{
           headerShown: false,
